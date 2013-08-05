a35ecef511e7fc05214ba779d6085cb8d361c8a7
Denis Rodin <denis.rodin@gmail.com>
2013-08-05 11:31:23 +0400

    + new build of xButton lib (with mouse support).

--- external/xbutton.js
+++ external/xbutton.js
@@ -4,47 +4,54 @@
  */
 
 (function(){
+    var isTouchPad = (/hp-tablet/gi).test(navigator.appVersion),
+        hasTouch = 'ontouchstart' in window && !isTouchPad,
+        START_EV = hasTouch ? 'touchstart' : 'mousedown',
+        MOVE_EV = hasTouch ? 'touchmove' : 'mousemove',
+        END_EV = hasTouch ? 'touchend' : 'mouseup',
+        CANCEL_EV = hasTouch ? 'touchcancel' : 'mouseup';
+
     var XButton = function(el, relations, handler, btnDownClass){
         var btn = this.element = typeof el == 'object' ? el : document.getElementById(el);;
         btn.btnData = {};
         btn.btnData.relations = relations;
         btn.btnData.style = btnDownClass;
         btn.btnData.handler = handler;
-        btn.addEventListener('touchstart', touchHandler, false);
-        btn.addEventListener('touchmove', touchHandler, false);
-        btn.addEventListener('touchend', touchHandler, false);
+        btn.addEventListener(START_EV, touchHandler, false);
+        btn.addEventListener(MOVE_EV, touchHandler, false);
+        btn.addEventListener(END_EV, touchHandler, false);
     };
 
     XButton.prototype = {
         destroy : function() {
             this.element.btnData = null;
-            this.element.removeEventListener('touchstart', touchHandler, false );
-            this.element.removeEventListener('touchmove', touchHandler, false );
-            this.element.removeEventListener('touchend', touchHandler, false );
+            this.element.removeEventListener(START_EV, touchHandler, false );
+            this.element.removeEventListener(MOVE_EV, touchHandler, false );
+            this.element.removeEventListener(END_EV, touchHandler, false );
         }
     };
 
     function touchHandler(e) {
         var data = this.btnData;
-        if (e.type == 'touchstart') {
+        if (e.type == START_EV) {
             addClass(data.style, this);
             data.x = e.touches[0].pageX;
             data.y = e.touches[0].pageY;
             data.cancel = false;
-        } else if (e.type == 'touchmove') {
+        } else if (e.type == MOVE_EV) {
             var dx = Math.abs(data.x - e.touches[0].pageX);
             var dy = Math.abs(data.y - e.touches[0].pageY);
             if (dx>10 || dy > 10) {
                 data.cancel = true;
                 removeClass(data.style, this);
             }
-        } else if (e.type == 'touchend') {
+        } else if (e.type == END_EV) {
             if (!data.cancel) {
                 removeClass(data.style, this);
                 //execute handler
                 data.handler(data.relations);
             }
-        } else if (e.type == 'touchcancel') {
+        } else if (e.type == CANCEL_EV) {
             data.cancel = true;
             removeClass(data.style, this);
         }
